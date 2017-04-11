3b2eb98e824b3dc75efa6e0a8425fd614b8f0e6e
Anthony Leung <anthoynleung989@gmail.com>
2017-04-12 09:06:07 +1200

    Added results pages and enhanced index page

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function noResultsClick() {
+    swal({
+        title: "No Results",
+        text: "Please search for another pole",
+        type: "error",
+        confirmButtonColor: "#aac341"
+    });
+}
